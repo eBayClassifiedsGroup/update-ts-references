afc7d74d0b1f417981ca88b4926e41e85ed3d34f
Mirko Kruschke <mkruschke@team.mobile.de>
2021-04-05 21:01:44 +0000

    ci: fix windows test execution

---
+++ scripts/prepareTests.ps1
@@ -0,0 +1,6 @@
+Invoke-Expression "yarn link"
+Remove-Item .\test-run -Confirm:$false -Force -Recurse -ErrorAction Ignore
+Copy-Item .\test-scenarios -Destination .\test-run -Recurse
+$prevLocation = Get-Location
+Get-ChildItem .\test-run | ForEach-Object { Set-Location $_.FullName; Invoke-Expression "yarn"; Invoke-Expression "yarn link update-ts-references" }
+Set-Location $prevLocation
